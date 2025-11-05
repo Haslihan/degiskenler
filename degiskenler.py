bcf4a50aa2eb775abe2fb1160b92b7535191ee0a
Haslihan <haslihan8@gmail.com>
2025-11-05 15:35:24 +0300

    Add files via upload

---
+++ degiskenler.py
@@ -0,0 +1,8 @@
+Character_name = "Null"
+Character_nickname = "Final Boss"
+Character_power_stats = 97
+Character_speed_status = 98
+Character_durability_status = 94
+Character_active = True
+character = {Character_name:"Null", Character_nickname:"FinalBoss"}
+character_stats = {Character_power_stats:97,Character_speed_status:98,Character_durability_status:94,}
